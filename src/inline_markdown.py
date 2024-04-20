bf25eafb2fc06c0d28d5c32c86af2a7621734e12
remcous <67499364+remcous@users.noreply.github.com>
2024-04-19 22:12:03 -0700

    inline markdown parsing completed

---
+++ src/inline_markdown.py
@@ -0,0 +1,110 @@
+import re
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold, 
+    text_type_code,
+    text_type_italic,
+    text_type_image,
+    text_type_link
+)
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        if node.text.count(delimiter) % 2 != 0:
+            raise ValueError("invalid markdown syntax")
+        substr = node.text.split(delimiter)
+        temp = []
+        i = 0
+        if node.text[:len(delimiter)] != delimiter:
+            i += 1
+        for text in substr:
+            if text == "":
+                continue
+            if i % 2 == 0:
+                temp.append(TextNode(text, text_type))
+            else:
+                temp.append(TextNode(text, text_type_text))
+            i += 1
+        new_nodes.extend(temp)
+    return new_nodes
+
+def extract_markdown_images(text):
+    regexp = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(regexp, text)
+    return matches
+
+def extract_markdown_links(text):
+    regexp = r"[^!]\[(.*?)\]\((.*?)\)"
+    matches = re.findall(regexp, text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        if node.text == "":
+            continue
+        matches = extract_markdown_images(node.text)
+        if matches == None:
+            new_nodes.append(node)
+        else:
+            temp = node.text
+            for match in matches:
+                substrs = temp.split(f"![{match[0]}]({match[1]})", 1)
+                if substrs[0] != "":
+                    new_nodes.append(TextNode(substrs[0], text_type_text))
+                new_nodes.append(TextNode(match[0], text_type_image, match[1]))
+                temp = substrs[1]
+                
+            if temp != "":
+                new_nodes.append(TextNode(temp, text_type_text))
+            
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+            continue
+        if node.text == "":
+            continue
+        matches = extract_markdown_links(node.text)
+        if matches == None:
+            new_nodes.append(node)
+        else:
+            temp = node.text
+            for match in matches:
+                substrs = temp.split(f"[{match[0]}]({match[1]})", 1)
+                if substrs[0] != "":
+                    new_nodes.append(TextNode(substrs[0], text_type_text))
+                new_nodes.append(TextNode(match[0], text_type_link, match[1]))
+                temp = substrs[1]
+                
+            if temp != "":
+                new_nodes.append(TextNode(temp, text_type_text))
+            
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    
+    # Split BOLD
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    # Split ITALICS
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    # Split CODE
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    # Split IMAGES
+    nodes = split_nodes_image(nodes)
+    # Split LINKS
+    nodes = split_nodes_link(nodes)
+    
+    return nodes
---
+++ src/test_inline_markdown.py
@@ -0,0 +1,180 @@
+import unittest
+from inline_markdown import(
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes
+)
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link
+)
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word and ", text_type_text),
+                TextNode("another", text_type_bold),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_multiword(self):
+        node = TextNode(
+            "This is text with a **bolded word** and **another**", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded word", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("another", text_type_bold),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_italic(self):
+        node = TextNode("This is text with an *italic* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+        
+    def test_extract_images(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        result = extract_markdown_images(text)
+        expected = [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")]
+        self.assertEqual(result, expected)
+        
+    def test_extract_links1(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        result = extract_markdown_links(text)
+        expected = []
+        self.assertEqual(result, expected)
+        
+    def test_extract_links2(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        expected = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
+        result = extract_markdown_links(text)
+        self.assertEqual(result, expected)
+        
+    def test_split_nodes_image1(self):
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode(
+                "second image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+            ),
+        ]
+        
+        self.assertEqual(new_nodes, expected)
+    
+    def test_split_nodes_image2(self):
+        nodes = [TextNode(
+            "This node only contains text",
+            text_type_text,
+            ),
+            TextNode(
+                "",
+                text_type_text,
+            ),
+            TextNode(
+                "This text contains a [link](https://www.boot.dev)",
+                text_type_text,
+            )
+        ]
+        new_nodes = split_nodes_image(nodes)
+        expected = [
+            TextNode("This node only contains text",
+                     text_type_text),
+            TextNode("This text contains a [link](https://www.boot.dev)",
+                     text_type_text)
+        ]
+        
+        self.assertEqual(new_nodes, expected)
+    
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.example.com"),
+            TextNode(" and ", text_type_text),
+            TextNode("another", text_type_link, "https://www.example.com/another")
+        ]
+        
+        self.assertEqual(new_nodes, expected)
+
+    def test_text_to_textnode(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+        expected = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+        self.assertEqual(text_to_textnodes(text), expected)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
