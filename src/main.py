47e69be16a7a249fe1d43799b6971be018fdafaf
Remi Coussement <67499364+remcous@users.noreply.github.com>
2024-04-27 13:43:38 -0700

    recursive copy function completed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode
+import os
+import shutil
+from copystatic import copy_files_recursive
+
+static_path = "./static"
+public_path = "./public"
 
 def main():
-    tn = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(tn)
+    print("Deleting public directory...", end="")
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    print("complete")
+
+    print("Copying files from static to public directory...")
+    copy_files_recursive(static_path, public_path)
+
 
 if __name__ == "__main__":
     main()
